ddf5e379031c5371cf6918809f67dd748f2b9e42
Michael (Mike) Williams <34947926+mpw07458@users.noreply.github.com>
2018-10-08 14:41:03 -0400

    Update PY-Terra-K8S.py

--- Py-Terra-K8S/PY-Terra-K8S.py
+++ Py-Terra-K8S/PY-Terra-K8S.py
@@ -10,11 +10,11 @@ def test_get_APIRequests():
     :return:
     '''
 
-    tenant = os.environ['TENANT']
+    tenant = os.environ['ARM_TENANT_ID']
     authority_url = 'https://login.microsoftonline.com/' + tenant
-    client_id = os.environ['CLIENTID']
-    client_secret = os.environ['CLIENTSECRET']
-    resource = 'https://management.azure.com/'
+    client_id = os.environ['ARM_CLIENT_ID']
+    client_secret = os.environ['ARM_CLIENT_SECRET']
+    # subscription_id = os.environ['ARM_SUBSCRIPTION_ID']
     context = adal.AuthenticationContext(authority_url)
     token = context.acquire_token_with_client_credentials(resource, client_id, client_secret)
     headers = {'Authorization': 'Bearer ' + token['accessToken'], 'Content-Type': 'application/json'}
@@ -28,16 +28,45 @@ def test_get_APIRequests():
 
 
 def test_create_ResourceGroup():
-    '''
+    """
     Create a reqsource Groups with Azure RM API
     :return:
-    '''
-    tenant = os.environ['TENANT']
+    """
+    tenant = os.environ['ARM_TENANT_ID']
     authority_url = 'https://login.microsoftonline.com/' + tenant
-    client_id = os.environ['CLIENTID']
-    client_secret = os.environ['CLIENTSECRET']
+    client_id = os.environ['ARM_CLIENT_ID']
+    client_secret = os.environ['ARM_CLIENT_SECRET']
+    subscription_id = os.environ['ARM_SUBSCRIPTION_ID']
     resource = 'https://management.azure.com/'
     context = adal.AuthenticationContext(authority_url)
+    token = context.acquire_token_with_client_credentials(resource, client_id, client_secret)
+    headers = {'Authorization': 'Bearer ' + token['accessToken'], 'Content-Type': 'application/json'}
+    params = {'api-version': '2017-05-10'}
+    url = 'https://management.azure.com/subscriptions/' + subscription_id + '/resourcegroups/mpwtestrg'
+
+    data = {'location': 'eastus'}
+
+    r = requests.put(url, data=json.dumps(data), headers=headers, params=params)
+
+    print(json.dumps(r.json(), indent=4, separators=(',', ': ')))
+    return
+
+    def test_create_ServicePrincipal():
+    """
+    create a service principal via RM
+    """
+    pass
+
+    def test_create_StorageAccount():
+    """
+    create a service principal via RM
+    """
+    pass
+
+
+if __name__ == "__main__":
+    test_get_APIRequests()
+    test_create_ResourceGroup()
     token = context.acquire_token_with_client_credentials(resource, client_id, client_secret)
     headers = {'Authorization': 'Bearer ' + token['accessToken'], 'Content-Type': 'application/json'}
     params = {'api-version': '2017-05-10'}
