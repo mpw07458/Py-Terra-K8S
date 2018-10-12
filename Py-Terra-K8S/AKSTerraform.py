2143f20016398f7023cb95e5b0b2966dcf9dd541
Michael (Mike) Williams <34947926+mpw07458@users.noreply.github.com>
2018-10-12 09:27:55 -0400

    v 0.5 works with diction for variables
    v 0.5 works with diction for variables

---
+++ Py-Terra-K8S/AKSTerraform.py
@@ -0,0 +1,231 @@
+# from datetime import datetime
+import uuid
+import shutil
+import os
+import json
+import subprocess
+import sys
+# from subprocess import check_output
+
+class AKSTerraform():
+    def __init__(self, source_manifest_path, storage_account, storage_container, variables=None):
+
+
+        self.name = os.path.basename(source_manifest_path).split(".")[0]
+        # print("Manifest name:" + self.name)
+        self.object_id = str(uuid.uuid4())
+        self.source_manifest_path = source_manifest_path
+        print(self.source_manifest_path)
+        self.tmpdir = os.path.join(".", "." + self.object_id)
+        print(self.tmpdir)
+        self.manifestdir = os.path.join(self.tmpdir, self.name)
+        print(self.manifestdir)
+        self.state_path = os.path.join(self.manifestdir, "terraform.tfstate")
+        print(self.state_path)
+        self.AKS_State_str = self.set_AKS_State(storage_account, storage_container)
+        self.AKS_Plan_str = self.set_AKS_Plan("/out.plan")
+        self.AKS_Apply_str = self.set_AKS_Apply("/out.plan")
+        self.variables = ""
+        self.copy_tmp_manifest()
+        self.variables = self.parse_variables(variables)
+        print("Variables: ",variables)
+        self.generate_vars_file()
+        init(self)
+        # plan(self)
+        # apply(self)
+
+
+    def cleanup(self):
+        self.remove_tmp_manifest()
+
+    def is_a_terraform_file(self, f):
+        return (os.path.isfile(f) and (f.lower().endswith(".tf") or f.lower().endswith(".tf.json")))
+
+    def copy_tmp_manifest(self):
+        if os.path.isdir(self.source_manifest_path):
+            files = [f for f in os.listdir(self.source_manifest_path) if self.is_a_terraform_file(os.path.join(self.source_manifest_path, f))]
+            if len(files) > 0:
+                if not os.path.isdir(self.tmpdir):
+                    os.mkdir(self.tmpdir)
+                shutil.copytree(self.source_manifest_path, os.path.join(self.tmpdir, self.name))
+            else:
+                raise(Exception("Terraform files not found.  Must have at least one *.tf or *.tf.json file to feed to Terraform."))
+        elif self.is_a_terraform_file(self.source_manifest_path):
+            if not os.path.isdir(self.tmpdir):
+                os.mkdir(self.tmpdir)
+            if not os.path.isdir(self.manifestdir):
+                os.mkdir(self.manifestdir)
+            shutil.copy(self.source_manifest_path, self.manifestdir)
+        else:
+            raise(Exception("Manifest file not found.  Must have a *.tf or *.tf.json file to feed to Terraform."))
+
+    def generate_vars_file(self):
+        with open(os.path.join(self.manifestdir, 'variables.tf'), 'w') as vars_file:
+            vars_file.write(self.variables)
+        vars_file.close()
+
+    def remove_tmp_manifest(self):
+        if os.path.isdir(self.tmpdir):
+            shutil.rmtree(self.tmpdir)
+
+    def set_AKS_State(self, StorageAccount, StorageContainer):
+        account_Str = "-backend-config="  + "'" +  "storage_account_name=" + StorageAccount + "'"
+        container_Str = "-backend-config="  + "'" +  "container_name=" + StorageContainer + "'"
+        return account_Str + " " + container_Str
+
+    def set_AKS_Plan(self, PlanStr):
+        AKS_plan_str = "-out " + self.manifestdir + PlanStr
+        return AKS_plan_str
+
+    def set_AKS_Apply(self, ApplyStr):
+        AKS_apply_str = self.manifestdir + ApplyStr
+        return AKS_apply_str
+
+    def state(self):
+        retval = {}
+        if os.path.isfile(self.state_path):
+            with open(self.state_path, 'r') as state_file:
+                retval = json.load(state_file)
+            state_file.close()
+        return retval
+
+    def old_apply(self):
+        self.current_stats = {}
+        cmd = "terraform apply -input=false -state=" + self.state_path  + " " + self.manifestdir
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, bufsize=1, shell=True)
+        output = self.parse_output(p)
+        p.stdout.close()
+        return output
+
+    def destroy(self):
+        self.current_stats = {}
+        cmd = "terraform destroy -input=false -force -state=" + self.state_path  + " " + self.manifestdir
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, bufsize=1, shell=True)
+        output = self.parse_output(p)
+        p.stdout.close()
+        return output
+
+    def parse_output(self, process):
+        retval = {
+            "start_times": {},
+            "durations": {}
+        }
+        #for line in iter(process.stdout.readline, b''):
+            #if "Creating..." in line or "Destroying..." in line:
+                # object_name = line.split(": ")[0].replace('\x1b[0m\x1b[1m','')
+                #retval['start_times'][object_name] = datetime.utcnow()
+                # print line.strip() + ", starting at " + str(retval['start_times'][object_name]) + "\n"
+            #elif "Creation complete" in line or "Destruction complete" in line:
+                # object_name = line.split(": ")[0].replace('\x1b[0m\x1b[1m','')
+                #retval['durations'][object_name] = datetime.utcnow() - retval['start_times'][object_name]
+                # print line.strip() + ", with duration " + str(retval['durations'][object_name]) + "\n"
+            #else:
+                # print line
+        return retval
+
+    def parse_variables(self, variables):
+        if variables is not None:
+            tf_vars = ""
+            for variable in variables:
+                innerV = variables[variable]
+                print(variable, innerV)
+                if isinstance(innerV, int):
+                    tf_vars = tf_vars + "variable \"" + variable + "\" " + "{" + "\n" + "  default = " \
+                              + str(innerV) + "\n" + "}" + "\n" + "\n"
+                else:
+                    tf_vars = tf_vars + "variable \"" + variable + "\" " + "{" + "\n" + "  default = " \
+                              + "\"" + innerV + "\"" + "\n" + "}" + "\n" + "\n"
+            return tf_vars
+        else:
+            return None
+
+def init(self):
+    tf_cmd = "Terraform init" + " " + self.AKS_State_str + " " + self.manifestdir
+    print(tf_cmd)
+    tf_command_helper(tf_cmd)
+
+def plan(self):
+    tf_cmd = "Terraform plan" + " " + self.AKS_Plan_str + " " + self.manifestdir
+    print(tf_cmd)
+    tf_command_helper(tf_cmd)
+
+def apply(self):
+    tf_cmd = "Terraform apply" + " " + self.AKS_Apply_str
+    print(tf_cmd)
+    tf_command_helper(tf_cmd)
+
+###########################################################################################################
+# K8S Command helpers and builders provide an ability to wrap Command line and API calls for K8S
+# will create helper class
+###########################################################################################################
+def tf_command_builder(self, command):
+    """
+    Kubernetes Command Builder mpw v0.7
+    accepts kubernetes cluster command
+    returns custom command based on JSON values
+    :param self, command:
+    :return:
+    """
+    # create sub-command strings
+    # return fully qualified kubernetes shell commands
+    return {
+            'create': 'kops create cluster' + ' ' + self.SubDomain + ' ' + self.CloudType + ' ' + self.State + ' '
+                      + self.Zones + ' ' + self.NodeCount + ' ' + self.MasterSize + ' ' + self.NodeSize + ' '
+                      + self.DNSZones,
+            'delete': 'kops delete cluster' + ' ' + self.SubDomain + ' ' + self.State + ' ' + self.Yes,
+            'update': 'kops update cluster' + ' ' + self.SubDomain + ' ' + self.State + ' ' + self.Yes,
+            'validate': 'kops validate cluster' + ' ' + self.SubDomain + ' ' + self.State,
+            'rolling-update': 'kops rolling-update cluster' + ' ' + self.SubDomain + ' ' + self.State + ' '
+                              + self.Yes,
+            'get deployments': 'kubectl' + ' ' + command,
+            'get pods': 'kubectl' + ' ' + command,
+            'get nodes': 'kubectl' + ' ' + command,
+            'get rs': 'kubectl' + ' ' + command,
+            'version': 'kubectl' + ' ' + command,
+            'cluster-info': 'kubectl' + ' ' + command,
+            'config view': 'kubectl' + ' ' + command
+        }.get(command, "")
+
+def tf_command_helper_sys(cmd):
+    """
+    K8S_Shell_Driver Package mpw
+    mpw v0.7
+    Kubernetes Command Helper - sends shell scripts to Shell
+    Works at Linux command line or Powershell
+    Accepts filename and path and executes shell script
+    as subprocess shell command
+    :param cmd:
+    :return:
+    """
+    print(cmd)
+    try:
+      retcode = subprocess.call([cmd], shell=True)
+      if retcode < 0:
+          print(sys.stderr, "Child was terminated by signal", -retcode)
+      else:
+          print(sys.stderr, "Child returned", retcode)
+    except OSError as e:
+          print(sys.stderr, "Execution failed:", e)
+
+def tf_command_helper(cmd):
+    """
+    mpw v0.7
+    Kubernetes Command Helper - sends non-executable
+    shell commands to a command Shell
+    Works at Linux command line or Powershell
+    Accepts ankubectl run --image=nginx nginx-app --port=80 --env="DOMAIN=cluster"y non- OS executable command
+    and sends it as subprocess shell command
+    shell = True means this will routine handle
+    :param cmd:
+    :return:
+    """
+    print(cmd)
+    try:
+      retcode = subprocess.call([cmd], shell=True)
+      if retcode < 0:
+          print(sys.stderr, "Child was terminated by signal", -retcode)
+      else:
+          print(sys.stderr, "Child returned", retcode)
+    except OSError as e:
+        print(sys.stderr, "Execution failed:", e)
+
--- Py-Terra-K8S/PY-Terra-K8S.py
+++ Py-Terra-K8S/PY-Terra-K8S.py
@@ -1,20 +1,76 @@
+import json
+import os
 import adal
 import requests
-import os
-import json
+from AKSTerraform import *
+# import pterraform
+# from python_terraform import *
+from azure.common.credentials import ServicePrincipalCredentials
+from azure.mgmt.resource import ResourceManagementClient
+from azure.mgmt.storage import StorageManagementClient
+from azure.mgmt.storage.models import (
+    StorageAccountCreateParameters,
+    StorageAccountUpdateParameters,
+    Sku,
+    Kind
+)
+from haikunator import Haikunator
+# noinspection PyUnresolvedReferences
+
+EAST_US = 'eastus'
+GROUP_NAME = 'mpw-azure-group'
+STORAGE_ACCOUNT_NAME = Haikunator().haikunate(delimiter='')
+
+variables = {
+    "client_id": "6484bac5-b63d-4385-bf53-44ee8144ea83",
+    "client_secret": "a030758c-ca55-44f8-9585-412fddbf0196",
+    "agent_count": 3,
+    "ssh_public_key": "~/.ssh/id_rsa.pub",
+    "dns_prefix": "tfk8stest",
+    "cluster_name": "tfk8stest",
+    "resource_group_name": "tfs-rg",
+    "location": "East US"
+}
+
+
+def get_credentials():
+    subscription_id = os.environ['ARM_SUBSCRIPTION_ID']
+    credentials = ServicePrincipalCredentials(
+        client_id=os.environ['ARM_CLIENT_ID'],
+        secret=os.environ['ARM_CLIENT_SECRET'],
+        tenant=os.environ['ARM_TENANT_ID']
+    )
+    return credentials, subscription_id
+
+
+def print_item(group):
+    """Print an Azure object instance."""
+    print("\tName: {}".format(group.name))
+    print("\tId: {}".format(group.id))
+    print("\tLocation: {}".format(group.location))
+    print("\tTags: {}".format(group.tags))
+    if hasattr(group, 'properties'):
+        print_properties(group.properties)
+
+
+def print_properties(props):
+    """Print a ResourceGroup properties instance."""
+    if props and props.provisioning_state:
+        print("\tProperties:")
+        print("\t\tProvisioning State: {}".format(props.provisioning_state))
+    print("\n\n")
 
 
 def test_get_APIRequests():
-    '''
+    """
     Get all the API requests for Azure RM
     :return:
-    '''
-
+    """
     tenant = os.environ['ARM_TENANT_ID']
     authority_url = 'https://login.microsoftonline.com/' + tenant
     client_id = os.environ['ARM_CLIENT_ID']
     client_secret = os.environ['ARM_CLIENT_SECRET']
-    # subscription_id = os.environ['ARM_SUBSCRIPTION_ID']
+    resource = 'https://management.azure.com/'
     context = adal.AuthenticationContext(authority_url)
     token = context.acquire_token_with_client_credentials(resource, client_id, client_secret)
     headers = {'Authorization': 'Bearer ' + token['accessToken'], 'Content-Type': 'application/json'}
@@ -51,33 +107,138 @@ def test_create_ResourceGroup():
     print(json.dumps(r.json(), indent=4, separators=(',', ': ')))
     return
 
-    def test_create_ServicePrincipal():
-    """
-    create a service principal via RM
-    """
-    pass
 
-    def test_create_StorageAccount():
+def test_create_StorageAccount():
     """
-    create a service principal via RM
+    Create a reqsource Groups with Azure RM API
+    :return:
     """
-    pass
+    # setup credentails
+    """Storage management example."""
+    #
+    # Create the Resource Manager Client with an Application (service principal) token provider
+    #
+    credentials, subscription_id = get_credentials()
 
+    resource_client = ResourceManagementClient(credentials, subscription_id)
+    storage_client = StorageManagementClient(credentials, subscription_id)
+
+    # You MIGHT need to add Storage as a valid provider for these credentials
+    # If so, this operation has to be done only once for each credentials
+    resource_client.providers.register('Microsoft.Storage')
+
+    # Create Resource group
+    print('Create Resource Group')
+    resource_group_params = {'location': 'eastus'}
+    print_item(resource_client.resource_groups.create_or_update(GROUP_NAME, resource_group_params))
+
+    # Check availability
+    print('Check name availability')
+    bad_account_name = 'invalid-or-used-name'
+    availability = storage_client.storage_accounts.check_name_availability(bad_account_name)
+    print('The account {} is available: {}'.format(bad_account_name, availability.name_available))
+    print('Reason: {}'.format(availability.reason))
+    print('Detailed message: {}'.format(availability.message))
+    print('\n\n')
+
+    # Create a storage account
+    print('Create a storage account')
+    storage_async_operation = storage_client.storage_accounts.update(
+        GROUP_NAME, STORAGE_ACCOUNT_NAME,
+        StorageAccountCreateParameters(
+           sku=Sku("mpw-storage"),
+           kind=Kind.storage,
+           location='eastus'
+        )
+    )
+
+    storage_account = storage_async_operation.result()
+    print_item(storage_account)
+    print('\n\n')
+
+    # Get storage account properties
+    print('Get storage account properties')
+    storage_account = storage_client.storage_accounts.get_properties(
+        GROUP_NAME, STORAGE_ACCOUNT_NAME)
+    print_item(storage_account)
+    print("\n\n")
+
+    # List Storage accounts
+    print('List storage accounts')
+    for item in storage_client.storage_accounts.list():
+        print_item(item)
+    print("\n\n")
+
+    # List Storage accounts by resource group
+    print('List storage accounts by resource group')
+    for item in storage_client.storage_accounts.list_by_resource_group(GROUP_NAME):
+        print_item(item)
+    print("\n\n")
+
+    # Get the account keys
+    print('Get the account keys')
+    storage_keys = storage_client.storage_accounts.list_keys(GROUP_NAME, STORAGE_ACCOUNT_NAME)
+    storage_keys = {v.key_name: v.value for v in storage_keys.keys}
+    print('\tKey 1: {}'.format(storage_keys['key1']))
+    print('\tKey 2: {}'.format(storage_keys['key2']))
+    print("\n\n")
+
+    # Regenerate the account key 1
+    print('Regenerate the account key 1')
+    storage_keys = storage_client.storage_accounts.regenerate_key(
+        GROUP_NAME,
+        STORAGE_ACCOUNT_NAME,
+        'key1')
+    storage_keys = {v.key_name: v.value for v in storage_keys.keys}
+    print('\tNew key 1: {}'.format(storage_keys['key1']))
+    print("\n\n")
+
+    # Update storage account
+    print('Update storage account')
+    storage_account = storage_client.storage_accounts.update(
+        GROUP_NAME, STORAGE_ACCOUNT_NAME,
+        StorageAccountUpdateParameters(
+            sku=Sku("mpw-storage")
+        )
+    )
+    print_item(storage_account)
+    print("\n\n")
+
+    # Delete the storage account
+    print('Delete the storage account')
+    storage_client.storage_accounts.delete(GROUP_NAME, STORAGE_ACCOUNT_NAME)
+    print("\n\n")
+
+    # Delete Resource group and everything in it
+    print('Delete Resource Group')
+    delete_async_operation = resource_client.resource_groups.delete(GROUP_NAME)
+    delete_async_operation.wait()
+    print("Deleted: {}".format(GROUP_NAME))
+    print("\n\n")
+
+    # List usage
+    print('List usage')
+    for usage in storage_client.usage.list():
+        print('\t{}'.format(usage.name.value))
+
+    return
+
+def test_create_AKS_Cluster():
+    """
+    Create a reqsource Groups with Azure RM API
+    :return:
+    """
+    print("creating cluster")
+    aks_tf = AKSTerraform('/Users/michaelwilliams/Documents/GitHub/Py-Terra-K8S/Py-Terra-K8S/Terraform',
+                          'cloudshellstoragempw', 'mpwcontainer', variables)
+    print("initializing Terraform")
+    # AKSTerraform.init()
+    # AKSTerraform.plan()
+    # AKSTerraform.apply()
+    return
 
 if __name__ == "__main__":
     test_get_APIRequests()
-    test_create_ResourceGroup()
-    token = context.acquire_token_with_client_credentials(resource, client_id, client_secret)
-    headers = {'Authorization': 'Bearer ' + token['accessToken'], 'Content-Type': 'application/json'}
-    params = {'api-version': '2017-05-10'}
-    url = 'https://management.azure.com/subscriptions/<subscription_id>/resourcegroups/mytestrg'
-
-    data = {'location': 'northeurope'}
-
-    r = requests.put(url, data=json.dumps(data), headers=headers, params=params)
-
-    print(json.dumps(r.json(), indent=4, separators=(',', ': ')))
-
-if __name__ == "__main__":
-    test_get_APIRequests()
-    test_create_ResourceGroup()
+    # test_create_ResourceGroup()
+    # test_create_StorageAccount()
+    test_create_AKS_Cluster()
