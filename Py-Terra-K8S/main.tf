c99ac8fc1030a99ba3c6ed12e3b95afc03ec4e65
Michael (Mike) Williams <34947926+mpw07458@users.noreply.github.com>
2018-10-18 21:59:03 -0400

    Add files via upload

---
+++ Py-Terra-K8S/main.tf
@@ -0,0 +1,21 @@
+provider "azurerm" {
+  version = "=1.15.0"
+}
+
+provider "helmcmd" {
+    "alias" = "kubernetes-stable"
+    "chart_source_type" = "repository"
+    # When chart_source_type is `repository`, this is the name of the repo
+    # whence to get the chart.
+    "chart_source" = "stable"
+    "debug" = true
+    "kube_context" = "OpenInnok8s"
+}
+
+resource "helmcmd_release" "plain" {
+    provider = "helmcmd.kubernetes-stable"
+    name = "ingress-controller"
+    chart_name = "nginx-ingress"
+    chart_version = "0.12.0"
+    namespace = "kube-system"
+}
